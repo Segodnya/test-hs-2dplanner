ce904ed4cde3e5e00b21ea7e8bd133f4dcc5d700
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-25 11:13:39 +0300

    Удален заготовка ObjectList

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import Board from "./Board";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import ObjectList from "./ObjectList";
 
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
-      <ObjectList />
       <Board />
     </DndProvider>
   );
